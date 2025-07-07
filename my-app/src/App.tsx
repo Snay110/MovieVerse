0625cd7950837760c0a8074187df5b3fff1f41cf
Oleksandr Murga <sanay8myrga@gmail.com>
2025-07-07 20:37:37 +0300

    feat: подключил Tailwindcss библиотеку

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,14 @@
 import MoviesList from "./moviesList";
-
+import './index.css'
 function App() {
   return (
     <>
       <MoviesList />
+      <h1 className="text-5xl font-extrabold underline text-pink-400 animate-pulse">
+        Tailwind работает! 💥
+      </h1>
+
+
     </>
   );
 }
